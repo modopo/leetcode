ceb4832121feeeb57a87af26586b50efb190bdde
Jonathan Tsai <modopo@gmail.com>
2023-08-31 20:23:10 -0700

    two pointer find min length to target

---
+++ array/minSubArrayLen.js
@@ -0,0 +1,37 @@
+/*
+Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+subarray
+ whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+Example 1:
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+Example 2:
+
+Input: target = 4, nums = [1,4,4]
+Output: 1
+Example 3:
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+
+*/
+
+function minSubArrayLen(target, nums) {
+  let min = nums.length + 1, end = 0, sum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    while (sum >= target) {
+      min = Math.min(min, i + 1 - end);
+      sum -= nums[end];
+      end++;
+    }
+  }
+
+  return (min != nums.length + 1) ? min : 0;
+}
+
+console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]))
