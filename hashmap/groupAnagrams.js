4462b1acfbb4ac58c75da21173871be51ec18eda
Jonathan Tsai <modopo@gmail.com>
2023-09-20 23:22:02 -0700

    storing anagrams

---
+++ hashmap/groupAnagrams.js
@@ -0,0 +1,11 @@
+/*
+Example 1: 49. Group Anagrams
+
+Given an array of strings strs, group the anagrams together.
+
+For example, given strs = ["eat","tea","tan","ate","nat","bat"], return [["bat"],["nat","tan"],["ate","eat","tea"]].
+*/
+
+function groupAnagrams(strs) {
+  
+}
