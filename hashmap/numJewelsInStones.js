a02126c29fd1f06f2b2a0e8ab6e35a89ef6eeef0
Jonathan Tsai <modopo@gmail.com>
2023-09-28 17:38:57 -0700

    checking if contains with hashmap

---
+++ hashmap/numJewelsInStones.js
@@ -0,0 +1,33 @@
+/*
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+*/
+
+function numJewelsInStones(jewels, stones) {
+  let jewelMap = new Map();
+
+  for (const jewel of jewels) {
+    jewelMap.set(jewel, 1);
+  }
+
+  let result = 0;
+  for (const stone of stones) {
+    if (jewelMap.has(stone)) {
+      result++;
+    }
+  }
+
+  return result;
+}
+
+console.log(numJewelsInStones('aA', 'aAABBBB'));
