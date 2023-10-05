64b272325dd30e1f134705e11f8ab1b0e752d375
Jonathan Tsai <modopo@gmail.com>
2023-10-05 00:09:24 -0700

    starting uniqueOccurrence

---
+++ hashmap/uniqueOccurrences.js
@@ -0,0 +1,36 @@
+// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+// Example 1:
+
+// Input: arr = [1,2,2,1,1,3]
+// Output: true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+// Example 2:
+
+// Input: arr = [1,2]
+// Output: false
+// Example 3:
+
+// Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+// Output: true
+
+function uniqueOccurrences(arr) {
+  let count = new Map();
+
+  for (const number of arr) {
+    count.set(number, (count.get(number) || 0) + 1) 
+  }
+
+  let unique = new Set();
+  for (const [key, val] of count.entries()) {
+    if (unique.has(val)) {
+      return false;
+    }
+
+    unique.add(val);
+  }
+
+  return true;
+}
+
+console.log(uniqueOccurrences([1,2]));
