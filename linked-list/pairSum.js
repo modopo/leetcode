ad47fd680f6dd21c0623109582f435308c4697b0
Jonathan Tsai <modopo@gmail.com>
2023-10-21 22:06:10 -0700

    pair sum practice

--- linked-list/pairSum.js
+++ linked-list/pairSum.js
@@ -1,42 +1,14 @@
-import LinkedList from "./linkedList.js";
+import ListNode from "./listNode.js";
 
-let test = new LinkedList();
-
-test.add(4);
-test.add(2);
-test.add(3);
-test.add(5);
+let test = new ListNode(4, new ListNode(2, new ListNode(3, new ListNode(6))));
 
 function pairSum(head) {
+  let slow = head, fast = head;
 
-  let front = head.head;
-  let slow = front;
-  let fast = front;
-  let middle = 0;
-
-  while (fast && fast.next) {
-    middle++;
+  while (fast.next && fast.next.next) {
     slow = slow.next;
     fast = fast.next.next;
-  }
+  };
 
-  let prev = null;
-  while (slow) {
-    let nextNode = slow.next;
-    slow.next = prev;
-    prev = slow;
-    slow = nextNode; 
-  }
 
-  let result = 0;
-  while (middle) {
-    result = Math.max(front.val + prev.val, result);
-    front = front.next;
-    prev = prev.next;
-    middle--;
-  }
-
-  return result;
 }
-
-console.log(pairSum(test));
