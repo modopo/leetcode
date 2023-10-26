9a171b8db852bf634c5d461edbbb491e74eff7b8
Jonathan Tsai <modopo@gmail.com>
2023-10-25 18:04:08 -0700

    in progress montonic stack

---
+++ stacks/makeGood.js
@@ -0,0 +1,55 @@
+// Given a string s of lower and upper case English letters.
+
+// A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+
+// 0 <= i <= s.length - 2
+// s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+// To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
+
+// Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+
+// Notice that an empty string is also good.
+
+// Example 1:
+
+// Input: s = "leEeetcode"
+// Output: "leetcode"
+// Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
+// Example 2:
+
+// Input: s = "abBAcC"
+// Output: ""
+// Explanation: We have many possible scenarios, and all lead to the same answer. For example:
+// "abBAcC" --> "aAcC" --> "cC" --> ""
+// "abBAcC" --> "abBA" --> "aA" --> ""
+// Example 3:
+
+// Input: s = "s"
+// Output: "s"
+
+
+function makeGood(s) {
+  if (s.length === 0 || s.length === 1) {
+    return s;
+  }
+  
+  let stack = [];
+
+  for (let char of s) {
+    let last = stack[stack.length - 1];
+
+    if (last === char) {
+      stack.push(char);
+    } else if (last === char.toUpperCase() || last === char.toLowerCase()) {
+      if (stack.length) {
+        stack.pop();
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.join('');
+}
+
+console.log(makeGood('leEeetcode'));
